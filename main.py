e9201650734036ee1bc5b8fb93fe15016391f9fe
Pliszka <karoliniakkjk@gmail.com>
2022-03-22 06:59:41 +0100

    log in

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+import os
+
+
+EMAIL = os.environ['lnd_email']
+PASSWORD = os.environ['lnd_password']
 
 chrome_driver_path = r'C:\Users\pluus\dev_stuff\chromedriver.exe'
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get('https://www.linkedin.com/jobs/search/?f_AL=true&f_E=1%2C2&f_PP=101832192&f_WT=1%2C2%2C3&keywords=python%20developer&sortBy=R')
-log_in = driver.find_element(By.XPATH, '/html/body/div[3]/header/nav/div/a[2]')
+log_button = driver.find_element(By.XPATH, '/html/body/div[3]/header/nav/div/a[2]')
+log_button.click()
+
+login = driver.find_element(By.XPATH, '//*[@id="username"]')
+login.send_keys(EMAIL)
+password_space = driver.find_element(By.XPATH, '//*[@id="password"]' )
+password_space.send_keys(PASSWORD)
+log_in = driver.find_element(By.XPATH, '//*[@id="organic-div"]/form/div[3]/button')
 log_in.click()
